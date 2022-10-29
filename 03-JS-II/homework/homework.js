9b56004e3d5554645bbf1d699f3a1c302d93116a
JossRz2013 <jossrz1988@gmail.com>
2022-10-29 02:01:43 -0500

    V5, homework.js

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -5,9 +5,10 @@ function obtenerMayor(x, y) {
   // Devuelve el número más grande
   // Si son iguales, devuelve cualquiera de los dos
   // Tu código:
-  if (x >= y){
-    return x;
-  } 
+if (x > y){
+  return x;
+}
+  return y;
 }
 
 function mayoriaDeEdad(edad) {
@@ -130,11 +131,10 @@ function fizzBuzz(numero) {
   // Si "numero" es divisible entre 5, devuelve "buzz"
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero
-     for (var i = 15; i < numero; i++){
-        if (i % 5 === 0 && i % 3 === 0) return "fizzbuzz"; 
-        else if (i % 10 === 0) return "buzz";
-        else if (i % 4 === 2) return "fizz";
-   } 
+  if(numero % 15 === 0) return 'fizzbuzz';
+  if(numero % 3 === 0) return 'fizz';
+  if(numero % 5 === 0) return 'buzz';
+  return numero;
 }
 
 function operadoresLogicos(num1, num2, num3) {
@@ -145,40 +145,22 @@ function operadoresLogicos(num1, num2, num3) {
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
 
-  if (num1 > 1 || num2 === -1 || num3 > 3){
-    return "Hay negativo";
-  } else {
-    if (num1 === 0 || num2 === 0 || num3 === 0){
-      return "Error";
-  } else {
-    if ((num1 > 0) && ((num1 > num2) && (num1 > num3))){
-      return "Número 1 es mayor y positivo";
-    } else {
-      if ((num3 > num1) && (num3 > num2)){
-      return num3++;
-    } else {
-        return false;
-       }
-     }
-    }
+    if(num1 < 0 || num2 < 0 || num3 < 0) {
+    return "Hay negativos";
   }
-}
-
-function esPrimo(numero) {
-  // Devuelve "true" si "numero" es primo
-  // De lo contrario devuelve "falso"
-  // Pista: un número primo solo es divisible por sí mismo y por 1
-  // Pista 2: Puedes resolverlo usando un bucle `for`
-  // Nota: Los números 0 y 1 NO son considerados números primos
-  if (numero < 2) return false;
-  if (numero === 2) return true;
-  for (var i = 2; i < numero; i++){
-    if (numero % i === 0){
-      return false;
-    }
+    else if(num1 === 0 || num2 === 0 || num3 === 0) {
+    return "Error";
   }
-    return true;
+    else if(num1 > 0 && num1 > num2 && num1 > num3) {
+    return "Número 1 es mayor y positivo";
   }
+    else if(num3 > num1 && num3 > num2) {
+    return num3 + 1;
+  }
+    else {
+     return false;
+   }
+ }
 
 function esVerdadero(valor){
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
@@ -216,11 +198,11 @@ function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Usar el bucle do ... while.
-   var a = numero;
+   var y = numero;
    var i = 0;
    do {
     i = i + 1;
-    a= a + 5;
+    y= y + 5;
     
    } while (i < 8);
      return a;
