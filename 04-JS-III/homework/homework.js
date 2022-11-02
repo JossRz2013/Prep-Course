af359cfb1a6375892ee0332a82e053cced3b28c9
JossRz2013 <jossrz1988@gmail.com>
2022-11-01 21:34:35 -0600

    last, homework.js

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -1,5 +1,6 @@
 // No cambies los nombres de las funciones.
 
+
 function devolverPrimerElemento(array) {
   // Devuelve el primer elemento de un  array (pasado por parametro)
   // Tu código:
@@ -62,6 +63,7 @@ function dePalabrasAFrase(palabras) {
       return saludos.join(" ");
 }
 
+
 function arrayContiene(array, elemento) {
   // Comprueba si el elemento existe dentro de "array"
   // Devuelve "true" si está, o "false" si no está
@@ -76,6 +78,7 @@ function arrayContiene(array, elemento) {
     }
   }
 }
+
 
 function agregarNumeros(numeros) {
   // "numeros" debe ser un arreglo de enteros (int/integers)
